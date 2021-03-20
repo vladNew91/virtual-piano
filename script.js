51beae35be32330c123c6577ed76e02b94508c7b
vladNew91 <vladnew91@gmail.com>
2021-03-21 02:39:26 +0300

    refactor: keyboard event

--- script.js
+++ script.js
@@ -32,34 +32,14 @@ btnFullscreen.addEventListener("click", () => {
 });
 
 //piano
-// arrayOfPianoKeys.forEach((elem, i) => {
-//     elem.addEventListener('mousedown', () => {
-//         arrayOfAudio[i].play();
-//         arrayOfAudio[i].currentTime = 0;
-//         elem.classList.add('piano-key-active-pseudo', 'piano-key-active');
-//     }, false);
-    
-//     elem.addEventListener('mouseup', () => {
-//         elem.classList.remove('piano-key-active-pseudo', 'piano-key-active');
-//     });
-
-//     elem.addEventListener('mouseover', () => {
-//         arrayOfAudio[i].play();
-//         arrayOfAudio[i].currentTime = 0;
-//         elem.classList.add('piano-key-active-pseudo', 'piano-key-active');
-//     });
-
-//     elem.addEventListener('mouseout', () => {
-//         elem.classList.remove('piano-key-active-pseudo', 'piano-key-active');
-//     }, false);
-// });
-
 const arrayOfAudio = document.querySelectorAll('audio');
 const pianoKey = document.querySelectorAll('.piano-key');
 const pianoBlock = document.querySelector('main');
 
 const playSound = (event) => {
     event.target.classList.add('piano-key-active-pseudo', 'piano-key-active');
+    // arrayOfAudio[arrayOfPianoKeys.indexOf(event.target)].play();
+    // arrayOfAudio[arrayOfPianoKeys.indexOf(event.target)].currentTime = 0;
 };
 
 const stopSound = (event) => {
@@ -74,7 +54,7 @@ const mouseDown = (event) => {
     arrayOfPianoKeys.forEach((el, i) => {
         el.addEventListener('mouseover', playSound);
         el.addEventListener('mouseout', stopSound);
-        if (el.classList.contains('piano-key-active-pseudo', 'piano-key-active')) {
+        if (el.classList.contains('piano-key-active-pseudo')) {
             arrayOfAudio[i].play();
             arrayOfAudio[i].currentTime = 0;
         }
@@ -93,174 +73,23 @@ pianoBlock.addEventListener('mousedown', mouseDown, false);
 pianoBlock.addEventListener('mouseup', mouseUp);
 
 //keyboard
-// const arrayKeyboardKeys = {
-//     key1: "KeyD",
-//     key2: "KeyF",
-//     key3: "KeyG",
-//     key4: "KeyH",
-//     key5: "KeyJ",
-//     key6: "KeyK",
-//     key7: "KeyL",
-//     key8: "KeyR",
-//     key9: "KeyT",
-//     key9: "KeyT",
-//     key10: "KeyU",
-//     key11: "KeyI",
-//     key12: "KeyO"
-// }
-//I KNOW ITS SHIT)
+const arrayKeyboardKeys = ["KeyD", "KeyF", "KeyG", "KeyH", "KeyJ", 
+    "KeyK", "KeyL", "KeyR", "KeyT", "KeyT","KeyU", "KeyI", "KeyO"];
+
+
 window.addEventListener('keydown', (event) => {
-    if(event.code == "KeyD") {
-        arrayOfPianoKeys[0].classList.add('piano-key-active-pseudo', 'piano-key-active')
-        arrayOfAudio[0].play();
+    if(arrayKeyboardKeys.includes(event.code)) {
+        let numberOfIndex = arrayKeyboardKeys.indexOf(event.code);
+        arrayOfPianoKeys[numberOfIndex].classList.add('piano-key-active-pseudo', 
+            'piano-key-active');
+        arrayOfAudio[numberOfIndex].play();
     }
 });
 
 window.addEventListener('keyup', (event) => {
-    if(event.code == "KeyD") {
-        arrayOfPianoKeys[0].classList.remove('piano-key-active-pseudo', 'piano-key-active')
-    }
-});
-
-window.addEventListener('keydown', (event) => {
-    if(event.code == "KeyF") {
-        arrayOfPianoKeys[1].classList.add('piano-key-active-pseudo', 'piano-key-active')
-        arrayOfAudio[1].play();
-    }
-});
-
-window.addEventListener('keyup', (event) => {
-    if(event.code == "KeyF") {
-        arrayOfPianoKeys[1].classList.remove('piano-key-active-pseudo', 'piano-key-active')
-    }
-});
-
-window.addEventListener('keydown', (event) => {
-    if(event.code == "KeyG") {
-        arrayOfPianoKeys[2].classList.add('piano-key-active-pseudo', 'piano-key-active')
-        arrayOfAudio[2].play();
-    }
-});
-
-window.addEventListener('keyup', (event) => {
-    if(event.code == "KeyG") {
-        arrayOfPianoKeys[2].classList.remove('piano-key-active-pseudo', 'piano-key-active')
-    }
-});
-
-window.addEventListener('keydown', (event) => {
-    if(event.code == "KeyH") {
-        arrayOfPianoKeys[3].classList.add('piano-key-active-pseudo', 'piano-key-active')
-        arrayOfAudio[3].play();
-    }
-});
-
-window.addEventListener('keyup', (event) => {
-    if(event.code == "KeyH") {
-        arrayOfPianoKeys[3].classList.remove('piano-key-active-pseudo', 'piano-key-active')
-    }
-});
-
-window.addEventListener('keydown', (event) => {
-    if(event.code == "KeyJ") {
-        arrayOfPianoKeys[4].classList.add('piano-key-active-pseudo', 'piano-key-active')
-        arrayOfAudio[4].play();
-    }
-});
-
-window.addEventListener('keyup', (event) => {
-    if(event.code == "KeyJ") {
-        arrayOfPianoKeys[4].classList.remove('piano-key-active-pseudo', 'piano-key-active')
-    }
-});
-
-window.addEventListener('keydown', (event) => {
-    if(event.code == "KeyK") {
-        arrayOfPianoKeys[5].classList.add('piano-key-active-pseudo', 'piano-key-active')
-        arrayOfAudio[5].play();
-    }
-});
-
-window.addEventListener('keyup', (event) => {
-    if(event.code == "KeyK") {
-        arrayOfPianoKeys[5].classList.remove('piano-key-active-pseudo', 'piano-key-active')
-    }
-});
-
-window.addEventListener('keydown', (event) => {
-    if(event.code == "KeyL") {
-        arrayOfPianoKeys[6].classList.add('piano-key-active-pseudo', 'piano-key-active')
-        arrayOfAudio[6].play();
-    }
-});
-
-window.addEventListener('keyup', (event) => {
-    if(event.code == "KeyL") {
-        arrayOfPianoKeys[6].classList.remove('piano-key-active-pseudo', 'piano-key-active')
-    }
-});
-
-window.addEventListener('keydown', (event) => {
-    if(event.code == "KeyR") {
-        arrayOfPianoKeys[7].classList.add('piano-key-active-pseudo', 'piano-key-active')
-        arrayOfAudio[7].play();
-    }
-});
-
-window.addEventListener('keyup', (event) => {
-    if(event.code == "KeyR") {
-        arrayOfPianoKeys[7].classList.remove('piano-key-active-pseudo', 'piano-key-active')
-    }
-});
-
-window.addEventListener('keydown', (event) => {
-    if(event.code == "KeyT") {
-        arrayOfPianoKeys[8].classList.add('piano-key-active-pseudo', 'piano-key-active')
-        arrayOfAudio[8].play();
-    }
-});
-
-window.addEventListener('keyup', (event) => {
-    if(event.code == "KeyT") {
-        arrayOfPianoKeys[8].classList.remove('piano-key-active-pseudo', 'piano-key-active')
-    }
-});
-
-window.addEventListener('keydown', (event) => {
-    if(event.code == "KeyU") {
-        arrayOfPianoKeys[10].classList.add('piano-key-active-pseudo', 'piano-key-active')
-        arrayOfAudio[10].play();
-    }
-});
-
-window.addEventListener('keyup', (event) => {
-    if(event.code == "KeyU") {
-        arrayOfPianoKeys[10].classList.remove('piano-key-active-pseudo', 'piano-key-active')
-    }
-});
-
-window.addEventListener('keydown', (event) => {
-    if(event.code == "KeyI") {
-        arrayOfPianoKeys[11].classList.add('piano-key-active-pseudo', 'piano-key-active')
-        arrayOfAudio[11].play();
-    }
-});
-
-window.addEventListener('keyup', (event) => {
-    if(event.code == "KeyI") {
-        arrayOfPianoKeys[11].classList.remove('piano-key-active-pseudo', 'piano-key-active')
-    }
-});
-
-window.addEventListener('keydown', (event) => {
-    if(event.code == "KeyO") {
-        arrayOfPianoKeys[12].classList.add('piano-key-active-pseudo', 'piano-key-active')
-        arrayOfAudio[12].play();
-    }
-});
-
-window.addEventListener('keyup', (event) => {
-    if(event.code == "KeyO") {
-        arrayOfPianoKeys[12].classList.remove('piano-key-active-pseudo', 'piano-key-active')
+    if(arrayKeyboardKeys.includes(event.code)) {
+        let numberOfIndex = arrayKeyboardKeys.indexOf(event.code);
+        arrayOfPianoKeys[numberOfIndex].classList.remove('piano-key-active-pseudo', 
+            'piano-key-active');
     }
 });
